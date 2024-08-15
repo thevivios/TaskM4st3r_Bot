dd855379e336ad80a240e9bdf6f357a106c38f34
thevivios <ilonaa881@gmail.com>
2024-08-16 00:57:52 +0300

    new

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,1 +1,10 @@
-'123'
+from aiogram import Bot, Dispatcher
+from dotenv import load_dotenv
+import os 
+
+load_dotenv()
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+
+bot = Bot(token=TELEGRAM_TOKEN)
+'1'
+
